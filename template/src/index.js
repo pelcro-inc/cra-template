e130e66b19735f2ca334ee7002bf197f4035f0b5
Khaled Albarawy <khaled.albarawi@gmail.com>
2021-08-22 21:30:58 +0200

    ci: use env vars for pelcro config

--- template/src/index.js
+++ template/src/index.js
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// Edit your Pelcro config in ./.env file
+
 let Pelcro = window.Pelcro || (window.Pelcro = {});
+
 // Your pelcro site ID
-Pelcro.siteid = "374";
+Pelcro.siteid = process.env.REACT_APP_PELCRO_SITE_ID;
 Pelcro.environment = {
-  // Replace with a real stripe key once on production
-  stripe: "pk_test_aThAAdvPHgIdAziZweywBWNk",
-  // Use "https://pelcro.com" for the production account
-  domain: "https://staging.pelcro.com",
+  stripe: process.env.REACT_APP_STRIPE_KEY,
+  // 'staging' || 'production'
+  domain:
+    process.env.REACT_APP_PELCRO_ENVIRONMENT === "production"
+      ? "https://www.pelcro.com"
+      : "https://staging.pelcro.com",
   // empty as we're using React as the UI bundle
   ui: " ",
 };
