12cd31998b0538a22c644503b3bf7a105c81d7ed
Khaled Albarawy <khaled.albarawi@gmail.com>
2021-09-06 21:36:46 +0200

    ci: update elements

--- template/src/index.js
+++ template/src/index.js
@@ -6,19 +6,6 @@ import App from "./App";
  *  Edit your Pelcro config in the .env file
  */
 
-let Pelcro = window.Pelcro || (window.Pelcro = {});
-
-Pelcro.siteid = process.env.REACT_APP_PELCRO_SITE_ID;
-Pelcro.environment = {
-  stripe: process.env.REACT_APP_STRIPE_KEY,
-  domain:
-    process.env.REACT_APP_PELCRO_ENVIRONMENT === "production"
-      ? "https://www.pelcro.com"
-      : "https://staging.pelcro.com",
-  // empty as we're using React as the UI bundle
-  ui: " ",
-};
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
